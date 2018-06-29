4d8f633532563ba2cb339b43bc63e4529f8012ab
Yurii Fisakov <fisakov.root@gmail.com>
2018-06-29 22:49:04 +0300

    added lb ip to outputs

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,7 +1,3 @@
-output "app_external_ip" {
-  value = "${google_compute_instance.app.network_interface.0.access_config.0.assigned_nat_ip}"
-}
-
-output "image_family" {
-  value = "${google_compute_instance.app.boot_disk.0.initialize_params.0.image}"
+output "lb_external_ip" {
+  value = "${google_compute_forwarding_rule.default.ip_address}"
 }
