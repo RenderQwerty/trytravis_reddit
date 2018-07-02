dc45f099749bb471835c310ac6598e777578f509
Yurii Fisakov <fisakov.root@gmail.com>
2018-07-02 14:25:27 +0300

    module structure initialized

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,60 +4,20 @@ provider "google" {
   region  = "${var.region}"
 }
 
-resource "google_compute_project_metadata" "default" {
-  metadata {
-    ssh-keys = "appuser1:${file(var.public_key_path)}appuser2:${file(var.public_key_path)}"
-  }
+module "app" {
+  source          = "modules/app"
+  public_key_path = "${var.public_key_path}"
+  zone            = "${var.zone}"
+  app_disk_image  = "${var.app_disk_image}"
 }
 
-resource "google_compute_instance" "app" {
-  name         = "reddit-app-${count.index}"
-  machine_type = "g1-small"
-  zone         = "${var.zone}"
-  tags         = ["reddit-app"]
-  count        = "${var.instance_count}"
-
-  metadata {
-    ssh-keys = "appuser:${file(var.public_key_path)}"
-  }
-
-  boot_disk {
-    initialize_params {
-      image = "${var.disk_image}"
-    }
-  }
-
-  network_interface = {
-    network       = "default"
-    access_config = {}
-  }
-
-  connection {
-    type        = "ssh"
-    user        = "appuser"
-    agent       = "false"
-    private_key = "${file(var.private_key_path)}"
-  }
-
-  provisioner "file" {
-    source      = "files/puma.service"
-    destination = "/tmp/puma.service"
-  }
-
-  provisioner "remote-exec" {
-    script = "files/deploy.sh"
-  }
+module "db" {
+  source          = "modules/db"
+  public_key_path = "${var.public_key_path}"
+  zone            = "${var.zone}"
+  db_disk_image   = "${var.db_disk_image}"
 }
 
-resource "google_compute_firewall" "firewall_puma" {
-  name    = "allow-puma-default"
-  network = "default"
-
-  allow {
-    protocol = "tcp"
-    ports    = ["9292"]
-  }
-
-  source_ranges = ["0.0.0.0/0"]
-  target_tags   = ["reddit-app"]
+module "vpc" {
+  source = "modules/vpc"
 }
---
+++ terraform/modules/vpc/variables.tf
@@ -0,0 +1,4 @@
+variable "ssh_port" {
+  description = "SSH custom port"
+  default     = "22"
+}
