6c8a549a7c74b4a6c937d5a1adfdea0e3eccb9d3
Yurii Fisakov <fisakov.root@gmail.com>
2018-07-03 19:08:34 +0300

    switched from direct script call to inline remote-exec provisioner

--- terraform/modules/db/main.tf
+++ terraform/modules/db/main.tf
@@ -23,7 +23,11 @@ resource "google_compute_instance" "db" {
   }
 
   provisioner "remote-exec" {
-    script = "${path.module}/files/db.sh"
+    #script = "${path.module}/files/db.sh"
+    inline = [
+      "sudo sed -i 's/bindIp: 127.0.0.1/bindIp: 0.0.0.0/g' /etc/mongod.conf",
+      "sudo systemctl restart mongod",
+    ]
   }
 
   metadata {
